83149b3d3078ec07555522f7e8845eb3f7da109b
Velda Kiara <veldakarimi@gmail.com>
2020-05-15 03:36:16 +0300

    zipped and completed pizza joint dets

--- lens_slice.py
+++ lens_slice.py
@@ -2,3 +2,6 @@
 toppings = ['pepperoni','pineapple','cheese','sausage','olives','anchovies','mushrooms']
 prices = [2,6,1,3,2,7,2]
 num_pizzas = len(toppings)
+print('We sell {} different kinds of pizza!'.format(num_pizzas))
+pizzas = list(zip(toppings,prices))
+print(pizzas)
