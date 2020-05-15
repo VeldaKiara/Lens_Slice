d8e2c459855676a9fb4aee1a6a78ae272508b4c7
Velda Kiara <veldakarimi@gmail.com>
2020-05-15 04:10:08 +0300

    done, done did it

--- lens_slice.py
+++ lens_slice.py
@@ -3,8 +3,15 @@ toppings = ['pepperoni','pineapple','cheese','sausage','olives','anchovies','mus
 prices = [2,6,1,3,2,7,2]
 num_pizzas = len(toppings)
 print('We sell {} different kinds of pizza!'.format(num_pizzas))
-pizzas = list(zip(toppings,prices))
+pizzas = list(zip(prices, toppings))
 print(pizzas)
 pizzas.sort()
+print(pizzas)
 cheapest_pizza = pizzas[0]
+print(cheapest_pizza)
 priciest_pizza = pizzas[-1]
+print(priciest_pizza)
+three_cheapest = pizzas[:3]
+print(three_cheapest)
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
